eedc51192748d6751276ee0e8ad37647edb71309
Luccas Galvan <50426126+LuccasGalvan@users.noreply.github.com>
2026-01-02 03:49:46 +0000

    Add account type/status fields to users

--- Trabalho/RESTFULLAPI/RESTfulAPIPWEB/Data/ApplicationUser.cs
+++ Trabalho/RESTFULLAPI/RESTfulAPIPWEB/Data/ApplicationUser.cs
@@ -1,14 +1,26 @@
 using Microsoft.AspNetCore.Identity;
 using RESTfulAPIPWEB.Entity.Enums;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace RESTfulAPIPWEB.Data
 {
+    public enum TipoConta { Cliente, Fornecedor, Admin, Funcionario }
+    public enum EstadoConta { Pendente, Ativo, Inativo }
+
     public class ApplicationUser : IdentityUser
     {
         public UserEstado Estado { get; set; } = UserEstado.Pendente;
-        public string? Nome { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; } = string.Empty;
+
+        [StringLength(100)]
         public string? Apelido { get; set; }
+
+        public TipoConta? TipoConta { get; set; }
+        public EstadoConta EstadoConta { get; set; } = EstadoConta.Pendente;
         public long? NIF { get; set; }
         public string? Rua { get; set; }
         public string? Localidade1 { get; set; }
