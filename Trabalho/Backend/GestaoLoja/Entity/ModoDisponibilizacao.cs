5edfe73c423f631e96fb65382e0224913daf40ac
Luccas Galvan <50426126+LuccasGalvan@users.noreply.github.com>
2026-01-02 04:35:07 +0000

    Add modo disponibilizacao entity

---
+++ Trabalho/Backend/GestaoLoja/Entity/ModoDisponibilizacao.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GestaoLoja.Entity
+{
+    public class ModoDisponibilizacao
+    {
+        public int Id { get; set; }
+
+        public bool IsForSale { get; set; }
+
+        public bool Ativo { get; set; }
+
+        [JsonIgnore]
+        public ICollection<Produto>? Produtos { get; set; }
+    }
+}
